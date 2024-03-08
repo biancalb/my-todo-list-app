fc3b9ab531d907cd667c7a3cf2348bb246c69b15
Bianca <biancalbarbosa@hotmail.com>
2024-03-07 23:06:06 -0300

    Add home page style

--- src/Pages/Home/Home.tsx
+++ src/Pages/Home/Home.tsx
@@ -1,10 +1,26 @@
 import React from 'react'
+import './Home.css'
+import Note from '../../Components/Note/Note'
 
 interface Props {}
 
 const Home = (props: Props) => {
   return (
-    <div>Home</div>
+    <>
+      <div className="header__navigation"></div>
+      <div className="date__picker"></div>
+      <div className="main__content">
+        <div className="notes__list">
+          <Note/>
+        </div>
+        <div className="addNew__button">
+            <a href="">
+                <button>+</button>
+            </a>
+        </div>
+      </div>
+      <footer></footer>
+    </>
   )
 }
 
