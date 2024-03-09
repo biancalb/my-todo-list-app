f29e808613557b04d6df99eaf5b23a68df0e4d94
Bianca <biancalbarbosa@hotmail.com>
2024-03-08 22:51:54 -0300

    Add -new note feature

--- src/Pages/Home/Home.tsx
+++ src/Pages/Home/Home.tsx
@@ -4,30 +4,58 @@ import Note from '../../Components/Note/Note';
 import { INote } from '../../ts/interfaces';
 import notes from '../../data/data.json';
 import Header from '../../Components/Header/Header';
-import DatePicker from '../../Components/DateSelector/DateSelector';
+import DateSelector from '../../Components/DateSelector/DateSelector';
 import { DateRange } from 'rsuite/esm/DateRangePicker';
+import Modal from 'react-bootstrap/Modal';
+import Button from 'react-bootstrap/Button';
+import { DatePicker } from 'rsuite';
+import moment from 'moment';
 
 const Home = () => {
+  // NOTES
   const [note, setNote] = useState<INote[]>();
+  useEffect(() => {
+    setNote(notes);
+  }, [notes]);
 
+  //RANGE PICKER
   const [value, setValue] =  useState<DateRange | null>([
     new Date(),
     new Date(new Date().setDate(new Date().getDate() + 7))
   ]); 
-
   const handleOnChange = (value:DateRange | null) => {
-    // console.log(value);
     setValue(value);
   };
 
-  useEffect(() => {
-    setNote(notes);
-  }, []);
+  // MODAL
+  const [show, setShow] = useState(false);
+  const handleClose = () => setShow(false);
+  const handleShow = () => {
+    setShow(true);
+    setNewNoteDate(null);
+  };
+  
+  // NEW NOTE
+  const [newNoteDate, setNewNoteDate] = useState<Date | null>();
+  const handleNewNoteDateOnChange = (value:Date | null) => {
+    setNewNoteDate(value);
+  };
+
+  const onSave = () => {
+    if (newNoteDate) {
+      notes.push({ 
+        'id': 4, 
+        'date': moment(newNoteDate).format('yyyy-MM-DD'), 
+        'tasks': [] 
+      });
+      setShow(false);
+    }
+  };
 
   return (
     <>
       <Header/>
-      <DatePicker value={value} handleOnChange={handleOnChange}/>
+      <DateSelector value={value} handleOnChange={handleOnChange}/>
       <div className="main__content">
         <div className="notes__list">
           {note?.map((n:INote) =>{
@@ -37,12 +65,35 @@ const Home = () => {
           })}
         </div>
         <div className="addNew__button">
-          <a href="">
-            <button>+</button>
-          </a>
+          <button onClick={handleShow}>+</button>
         </div>
       </div>
       <footer></footer>
+
+      <Modal
+        show={show}
+        onHide={handleClose}
+        backdrop="static"
+        centered
+      >
+        <Modal.Header closeButton>
+          <Modal.Title as="h6">Add new note</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <span className='modal-body__label'> Select the date </span>
+          <DatePicker
+            oneTap
+            format="dd/MM/yyyy"
+            value={newNoteDate}
+            onChange={(value) => handleNewNoteDateOnChange(value) }
+          />
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" size="sm" onClick={() => onSave()}>
+            Save
+          </Button>
+        </Modal.Footer>
+      </Modal>
     </>
   );
 };
--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,7 @@ import './index.css';
 import reportWebVitals from './reportWebVitals';
 import { RouterProvider } from 'react-router-dom';
 import { router } from './Routes/Route';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
